4d1dda772cbdf37ca13f3ec7fb50b6b3833d2811
이찬희 <iamchanii.0310@gmail.com>
2022-10-15 21:07:31 +0900

    Add lazyGreeting field

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import { buildSchema } from 'graphql';
 const typeDefs = /* GraphQL */ `
   type Query {
     greeting: String!
+    lazyGreeting: String!
   }
 `;
 
@@ -11,12 +12,20 @@ const schema = buildSchema(typeDefs, {
   enableDeferStream: true,
 });
 
+const sleep = (ms = 1000) => new Promise(resolve => setTimeout(resolve, ms));
+
 const server = createServer({
   schema: {
     typeDefs: schema,
     resolvers: {
       Query: {
         greeting: () => 'Hi!',
+
+        lazyGreeting: async () => {
+          await sleep();
+
+          return "...Oh, Hi! I don't know you were here.";
+        },
       },
     },
   },
