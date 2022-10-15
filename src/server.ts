ef19c1d275b66e03bc1ddacf05fa943fc3db519f
이찬희 <iamchanii.0310@gmail.com>
2022-10-15 21:01:16 +0900

    Build a schema using graphql's buildSchema API and set enableDeferStream true

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,19 @@
 import { createServer } from '@graphql-yoga/node';
+import { buildSchema } from 'graphql';
+
+const typeDefs = /* GraphQL */ `
+  type Query {
+    greeting: String!
+  }
+`;
+
+const schema = buildSchema(typeDefs, {
+  enableDeferStream: true,
+});
 
 const server = createServer({
   schema: {
-    typeDefs: /* GraphQL */ `
-      type Query {
-        greeting: String!
-      }
-    `,
+    typeDefs: schema,
     resolvers: {
       Query: {
         greeting: () => 'Hi!',
